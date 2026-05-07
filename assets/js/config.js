ffaec660fb345c833a14179fc2f28f2dc588f4e6
demomaytawin-crypto <demomaytawin@gmail.com>
2026-05-07 13:29:45 +0700

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,2 +0,0 @@
-// V6.1 config - ใส่ URL Apps Script Web App ตรงนี้
-var API_URL = 'https://script.google.com/macros/s/AKfycbzAeSmmBNeTFuheF83uDiHRVNoMUiQN7rDU0Gj_qRLEx880SR2kwSb0489s2OdrVfUUJQ/exec';
