446f0f6aa653d0921489e0b695dc5125ef6d523d
demomaytawin-crypto <demomaytawin@gmail.com>
2026-05-07 13:03:02 +0700

    Add API_URL for V6.1 config

---
+++ assets/js/config.js
@@ -0,0 +1,2 @@
+// V6.1 config - ใส่ URL Apps Script Web App ตรงนี้
+var API_URL = 'https://script.google.com/macros/s/AKfycbzAeSmmBNeTFuheF83uDiHRVNoMUiQN7rDU0Gj_qRLEx880SR2kwSb0489s2OdrVfUUJQ/exec';
